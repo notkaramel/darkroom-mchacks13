0209a5fe2dd9e63e2af7b2a6b5e3554a07d0879d
Antoine Phan <hoangtuan11102@gmail.com>
2026-01-17 13:44:15 -0500

    API stuff

--- application/src/routes/+page.svelte
+++ application/src/routes/+page.svelte
@@ -1,2 +1,16 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<div class="grid grid-cols-3 gap-4 h-screen w-screen">
+    <div class="col-span-1">
+    <!-- Selection Panel on the left side of the screen -->
+    </div>
+
+    <div class="col-span-1">
+    <!-- Picture View at the center of the screen -->   
+    </div>
+
+    <div class="col-span-1">
+    <!-- Editing Panel on the right side of the screen -->
+    </div>
+</div>
